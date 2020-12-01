ceed4b524273bb595d49f1230ebcf8fb9d07447d
zhouzhiwengang <zhouzhiwengang@163.com>
2020-12-01 18:08:25 +0800

    更新建库脚本

--- src/main/resources/sql/auth_shrio.sql
+++ src/main/resources/sql/auth_shrio.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 30/11/2020 18:42:35
+ Date: 01/12/2020 18:06:00
 */
 
 SET NAMES utf8mb4;
@@ -78,24 +78,39 @@ INSERT INTO `role_permission` VALUES (1, 3);
 INSERT INTO `role_permission` VALUES (1, 4);
 INSERT INTO `role_permission` VALUES (2, 4);
 
+-- ----------------------------
+-- Table structure for token_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `token_relation`;
+CREATE TABLE `token_relation`  (
+  `relation_sid` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户名',
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'token凭证',
+  PRIMARY KEY (`relation_sid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `user_id` int(11) NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `user_id` int(11) NOT NULL COMMENT '主键',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '账户',
+  `telephone` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号码',
+  `email` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱地址',
+  `guid` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '唯一码',
+  `real_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '真实姓名',
+  `address` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
   PRIMARY KEY (`user_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, '123', 'Jack');
-INSERT INTO `user` VALUES (2, '123', 'Rose');
-INSERT INTO `user` VALUES (3, '123', 'Paul');
+INSERT INTO `user` VALUES (1, '123', 'Jack', '123', '123@163.com', NULL, NULL, NULL);
+INSERT INTO `user` VALUES (2, '123', 'Rose', '1234', '1234@163.com', NULL, NULL, NULL);
+INSERT INTO `user` VALUES (3, '123', 'Paul', '12345', '12345@163.com', NULL, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for user_role
@@ -116,5 +131,18 @@ CREATE TABLE `user_role`  (
 INSERT INTO `user_role` VALUES (1, 1);
 INSERT INTO `user_role` VALUES (2, 2);
 INSERT INTO `user_role` VALUES (3, 3);
+
+-- ----------------------------
+-- Table structure for verification
+-- ----------------------------
+DROP TABLE IF EXISTS `verification`;
+CREATE TABLE `verification`  (
+  `verification_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `verification_code` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '验证码',
+  `status` tinyint(255) NULL DEFAULT NULL COMMENT '状态：1:未使用，2：已使用',
+  `create_date` datetime(0) NULL DEFAULT NULL COMMENT '创建日期',
+  `fail_date` datetime(0) NULL DEFAULT NULL COMMENT '失效日期',
+  PRIMARY KEY (`verification_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
