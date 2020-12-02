2b5a9a8def661a1bd2ecf96b4df9adbe775be9a1
zhouzhiwengang <zhouzhiwengang@163.com>
2020-12-02 18:46:45 +0800

    资源文件修改

--- src/main/resources/sql/auth_shrio.sql
+++ src/main/resources/sql/auth_shrio.sql
@@ -11,11 +11,100 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 01/12/2020 18:06:00
+ Date: 02/12/2020 18:45:00
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for cost
+-- ----------------------------
+DROP TABLE IF EXISTS `cost`;
+CREATE TABLE `cost`  (
+  `cost_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `house_id` int(11) NOT NULL COMMENT '房屋ID',
+  `create_date` date NOT NULL COMMENT '创建日期',
+  `money` int(11) NOT NULL DEFAULT 0 COMMENT '金额',
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '状态:1 未支付，2:已支付',
+  PRIMARY KEY (`cost_id`) USING BTREE,
+  INDEX `FK_cost_house`(`house_id`) USING BTREE,
+  CONSTRAINT `FK_cost_house` FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '费用信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of cost
+-- ----------------------------
+INSERT INTO `cost` VALUES (1, 2, '2020-12-02', 3360, 1);
+
+-- ----------------------------
+-- Table structure for house
+-- ----------------------------
+DROP TABLE IF EXISTS `house`;
+CREATE TABLE `house`  (
+  `house_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `house_number` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '房屋编号',
+  `area` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '区域',
+  `house_address` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '房屋地址',
+  `house_type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '房屋类型',
+  `acreage` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '房屋面积',
+  `peroples` int(10) NULL DEFAULT NULL COMMENT '可容纳人数',
+  `rent` int(10) NULL DEFAULT NULL COMMENT '房屋租金',
+  `status` tinyint(4) NULL DEFAULT NULL COMMENT '房屋状态:1:已租、2:待租',
+  `user_id` int(11) NOT NULL COMMENT '用户ID',
+  PRIMARY KEY (`house_id`) USING BTREE,
+  INDEX `FK_house_user`(`user_id`) USING BTREE,
+  CONSTRAINT `FK_house_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '房屋信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of house
+-- ----------------------------
+INSERT INTO `house` VALUES (1, '0001', '广东省宝安区麻布新村', '105号13-1101', '1', '28.35平方', 3, 1800, 1, 3);
+INSERT INTO `house` VALUES (2, '0002', '广东省宝安区麻布新村', '105号13-1102', '1', '44.2平方', 5, 3400, 1, 1);
+INSERT INTO `house` VALUES (4, '0003', '广东省宝安区麻布新村', '105号13-1103', '1', '38.35平方', 3, 2800, 1, 1);
+
+-- ----------------------------
+-- Table structure for house_lease
+-- ----------------------------
+DROP TABLE IF EXISTS `house_lease`;
+CREATE TABLE `house_lease`  (
+  `house_id` int(11) NOT NULL COMMENT '房屋ID',
+  `lease_id` int(11) NOT NULL COMMENT '租赁者ID',
+  PRIMARY KEY (`house_id`, `lease_id`) USING BTREE,
+  INDEX `FK_house_lease_lease`(`lease_id`) USING BTREE,
+  CONSTRAINT `FK_house_lease_house` FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `FK_house_lease_lease` FOREIGN KEY (`lease_id`) REFERENCES `lease` (`lease_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '房屋租赁信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of house_lease
+-- ----------------------------
+INSERT INTO `house_lease` VALUES (1, 1);
+INSERT INTO `house_lease` VALUES (2, 1);
+
+-- ----------------------------
+-- Table structure for lease
+-- ----------------------------
+DROP TABLE IF EXISTS `lease`;
+CREATE TABLE `lease`  (
+  `lease_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `real_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '租赁者姓名',
+  `id_card` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '租赁者身份证号码',
+  `sex` tinyint(4) NOT NULL COMMENT '性别：1 男 2 女',
+  `telphone` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '电话',
+  `hometown` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '租赁者籍贯',
+  `status` tinyint(4) NOT NULL COMMENT '状态:1:租房中  2:未租房',
+  `start_date` date NOT NULL COMMENT '租赁开始时间',
+  `end_date` date NOT NULL COMMENT '租赁结束时间',
+  `sign_date` date NOT NULL COMMENT '租赁签订时间',
+  PRIMARY KEY (`lease_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '租赁者信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of lease
+-- ----------------------------
+INSERT INTO `lease` VALUES (1, '周志刚', '430921199112205454', 1, '13265740591', '湖南', 1, '2015-10-01', '2025-01-01', '2020-12-02');
 
 -- ----------------------------
 -- Table structure for permission
@@ -35,6 +124,8 @@ INSERT INTO `permission` VALUES (1, 'select', '查看');
 INSERT INTO `permission` VALUES (2, 'update', '更新');
 INSERT INTO `permission` VALUES (3, 'delete', '删除');
 INSERT INTO `permission` VALUES (4, 'save', '新增');
+INSERT INTO `permission` VALUES (5, 'import', '导入');
+INSERT INTO `permission` VALUES (6, 'export-update', '导出-修改');
 
 -- ----------------------------
 -- Table structure for role
@@ -52,6 +143,7 @@ CREATE TABLE `role`  (
 INSERT INTO `role` VALUES (1, 'svip');
 INSERT INTO `role` VALUES (2, 'vip');
 INSERT INTO `role` VALUES (3, 'p');
+INSERT INTO `role` VALUES (4, 'admin');
 
 -- ----------------------------
 -- Table structure for role_permission
@@ -72,11 +164,17 @@ CREATE TABLE `role_permission`  (
 INSERT INTO `role_permission` VALUES (1, 1);
 INSERT INTO `role_permission` VALUES (2, 1);
 INSERT INTO `role_permission` VALUES (3, 1);
+INSERT INTO `role_permission` VALUES (4, 1);
 INSERT INTO `role_permission` VALUES (1, 2);
 INSERT INTO `role_permission` VALUES (2, 2);
+INSERT INTO `role_permission` VALUES (4, 2);
 INSERT INTO `role_permission` VALUES (1, 3);
+INSERT INTO `role_permission` VALUES (4, 3);
 INSERT INTO `role_permission` VALUES (1, 4);
 INSERT INTO `role_permission` VALUES (2, 4);
+INSERT INTO `role_permission` VALUES (4, 4);
+INSERT INTO `role_permission` VALUES (4, 5);
+INSERT INTO `role_permission` VALUES (4, 6);
 
 -- ----------------------------
 -- Table structure for token_relation
@@ -87,7 +185,12 @@ CREATE TABLE `token_relation`  (
   `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户名',
   `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'token凭证',
   PRIMARY KEY (`relation_sid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of token_relation
+-- ----------------------------
+INSERT INTO `token_relation` VALUES (3, 'Jack', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDY5NTkzNzAsInVzZXJuYW1lIjoiSmFjayJ9.m3OZ1w2YQLmI0cE9oK79_pZbXnON_-e5YMbZLPtdyrU');
 
 -- ----------------------------
 -- Table structure for user
@@ -111,6 +214,7 @@ CREATE TABLE `user`  (
 INSERT INTO `user` VALUES (1, '123', 'Jack', '123', '123@163.com', NULL, NULL, NULL);
 INSERT INTO `user` VALUES (2, '123', 'Rose', '1234', '1234@163.com', NULL, NULL, NULL);
 INSERT INTO `user` VALUES (3, '123', 'Paul', '12345', '12345@163.com', NULL, NULL, NULL);
+INSERT INTO `user` VALUES (4, '123', 'Jane', '123456', '123456@163.com', NULL, '简', '中国广东省深圳市盐田区');
 
 -- ----------------------------
 -- Table structure for user_role
@@ -128,9 +232,8 @@ CREATE TABLE `user_role`  (
 -- ----------------------------
 -- Records of user_role
 -- ----------------------------
-INSERT INTO `user_role` VALUES (1, 1);
-INSERT INTO `user_role` VALUES (2, 2);
 INSERT INTO `user_role` VALUES (3, 3);
+INSERT INTO `user_role` VALUES (4, 4);
 
 -- ----------------------------
 -- Table structure for verification
@@ -143,76 +246,6 @@ CREATE TABLE `verification`  (
   `create_date` datetime(0) NULL DEFAULT NULL COMMENT '创建日期',
   `fail_date` datetime(0) NULL DEFAULT NULL COMMENT '失效日期',
   PRIMARY KEY (`verification_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
-
-
--- 导出  表 auth_shrio.cost 结构
-CREATE TABLE IF NOT EXISTS `cost` (
-  `cost_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `house_id` int(11) NOT NULL COMMENT '房屋ID',
-  `create_date` date NOT NULL COMMENT '创建日期',
-  `money` int(11) NOT NULL DEFAULT '0' COMMENT '金额',
-  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态:1 未支付，2:已支付',
-  PRIMARY KEY (`cost_id`),
-  KEY `FK_cost_house` (`house_id`),
-  CONSTRAINT `FK_cost_house` FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='费用信息表';
-
--- 正在导出表  auth_shrio.cost 的数据：~0 rows (大约)
-/*!40000 ALTER TABLE `cost` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cost` ENABLE KEYS */;
-
--- 导出  表 auth_shrio.house 结构
-CREATE TABLE IF NOT EXISTS `house` (
-  `house_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `house_number` varchar(32) NOT NULL COMMENT '房屋编号',
-  `area` varchar(50) DEFAULT NULL COMMENT '区域',
-  `house_address` varchar(50) DEFAULT NULL COMMENT '房屋地址',
-  `house_type` varchar(50) DEFAULT NULL COMMENT '房屋类型',
-  `acreage` varchar(50) DEFAULT NULL COMMENT '房屋面积',
-  `peroples` int(10) DEFAULT NULL COMMENT '可容纳人数',
-  `rent` int(10) DEFAULT NULL COMMENT '房屋租金',
-  `status` tinyint(4) DEFAULT NULL COMMENT '房屋状态:1:已租、2:待租',
-  `user_id` int(11) NOT NULL COMMENT '用户ID',
-  PRIMARY KEY (`house_id`),
-  KEY `FK_house_user` (`user_id`),
-  CONSTRAINT `FK_house_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='房屋信息表';
-
--- 正在导出表  auth_shrio.house 的数据：~0 rows (大约)
-/*!40000 ALTER TABLE `house` DISABLE KEYS */;
-INSERT INTO `house` (`house_id`, `house_number`, `area`, `house_address`, `house_type`, `acreage`, `peroples`, `rent`, `status`, `user_id`) VALUES
-	(1, '0001', '广东省宝安区麻布新村', '105号13-1101', '1', '24.2平方', 3, 2400, 2, 1),
-	(2, '0002', '广东省宝安区麻布新村', '105号13-1102', '1', '44.2平方', 5, 3400, 1, 1);
-/*!40000 ALTER TABLE `house` ENABLE KEYS */;
-
--- 导出  表 auth_shrio.house_lease 结构
-CREATE TABLE IF NOT EXISTS `house_lease` (
-  `house_id` int(11) NOT NULL COMMENT '房屋ID',
-  `lease_id` int(11) NOT NULL COMMENT '租赁者ID',
-  PRIMARY KEY (`house_id`,`lease_id`),
-  KEY `FK_house_lease_lease` (`lease_id`),
-  CONSTRAINT `FK_house_lease_house` FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`),
-  CONSTRAINT `FK_house_lease_lease` FOREIGN KEY (`lease_id`) REFERENCES `lease` (`lease_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='房屋租赁信息表';
-
--- 正在导出表  auth_shrio.house_lease 的数据：~0 rows (大约)
-/*!40000 ALTER TABLE `house_lease` DISABLE KEYS */;
-/*!40000 ALTER TABLE `house_lease` ENABLE KEYS */;
-
--- 导出  表 auth_shrio.lease 结构
-CREATE TABLE IF NOT EXISTS `lease` (
-  `lease_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `real_name` varchar(64) NOT NULL COMMENT '租赁者姓名',
-  `id_card` varchar(64) NOT NULL COMMENT '租赁者身份证号码',
-  `sex` tinyint(4) NOT NULL COMMENT '性别：1 男 2 女',
-  `telphone` varchar(32) NOT NULL COMMENT '电话',
-  `hometown` varchar(128) NOT NULL COMMENT '租赁者籍贯',
-  `status` tinyint(4) NOT NULL COMMENT '状态:1:租房中  2:未租房',
-  `start_date` date NOT NULL COMMENT '租赁开始时间',
-  `end_date` date NOT NULL COMMENT '租赁结束时间',
-  `sign_date` date NOT NULL COMMENT '租赁签订时间',
-  PRIMARY KEY (`lease_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='租赁者信息表';
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
